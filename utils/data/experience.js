adf7c62ac47ea25295df9131fcf18e7d8f69acc1
Uday Shankar Paidimarri <udayshankarpaidimarri@Udays-MacBook-Air.local>
2024-12-06 23:57:54 -0500

    upodated

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -4,5 +4,6 @@ export const experiences = [
     title: "Software Engineer",
     company: "Nextel Meta Private Limited",
     duration: "(Jun 2021 - August 2022)",
+
   },
 ];
--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -3,7 +3,7 @@ export const personalData = {
   profile: "/profile.png",
   designation: "Software Developer",
   description:
-    "I am highly skilled full-stack developer with a Master's in Computer Science from George Mason University and a Bachelor's from Vellore Institute of Technology. With expertise in JavaScript, React, Next.js, and Spring Boot, Uday has a strong foundation in building dynamic web applications and optimizing backend systems. He has hands-on experience with microservices, cloud technologies, and modern web frameworks, gained through both academic projects and an internship at Nextel Meta. Uday is passionate about delivering efficient, scalable solutions in web development",
+    "I am a highly skilled full-stack developer with a Master's in Computer Science from George Mason University and a Bachelor's from Vellore Institute of Technology. With expertise in JavaScript, React, Next.js, and Spring Boot, Uday has a strong foundation in building dynamic web applications and optimizing backend systems. He has hands-on experience with microservices, cloud technologies, and modern web frameworks, gained through academic projects and professional experience at Nextel Meta. Uday is passionate about delivering efficient, scalable solutions in web development.",
   email: "udayshankar.2416@gmail.com",
   phone: "571-407-4428",
   address: "Fairfax,Virginia",
@@ -13,5 +13,5 @@ export const personalData = {
   twitter: "https://twitter.com/",
   stackOverflow: "https://stackoverflow.com/",
   leetcode: "https://leetcode.com/u/uday_shankar/",
-  resume: "https://drive.google.com/file/d/1W9eLs7N6lRcuu1D5649JXQ3PTp49EEfD/view?usp=drive_link",
+  resume: "https://drive.google.com/file/d/1moiiGG4EYmdKjlrpdyh36q0iXciqXqgR/view?usp=drive_link",
 };
